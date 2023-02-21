63e6b6f26208158814f97c5fc6914a56432bd56c
Dung1109 <Dung11092001@gmail.com>
2023-02-21 10:05:52 +0700

    Dung

--- src/fa/training/main/MyOwnAutoShop.java
+++ src/fa/training/main/MyOwnAutoShop.java
@@ -16,32 +16,40 @@ public class MyOwnAutoShop {
         int b = scanner.nextInt();
         System.out.println("Enter number of Seden: ");
         int c = scanner.nextInt();
-        System.out.println("Enter number of Car: " + (a+b+c));
-        int n = a+b+c;
+        System.out.println("Enter number of Car: " + (a + b + c));
+        int n = a + b + c;
         Car[] cars = new Car[n];
-        Truck[]  truck= new Truck[a];
-        Fold fold[] = new Fold[b];
-        Seden seden[] = new Seden[c];
-        for(int i=0;i<a;i++){
-            truck[i].inputInforCar(scanner);
+        //Create 3 array objects
+        Truck[] truck = new Truck[a];
+        Fold[] fold = new Fold[b];
+        Seden[] seden = new Seden[c];
+        for (int i = 0; i < a; i++) {
+            truck[i] = new Truck();
+            //Call input method
+            truck[i].inputInfoCar(scanner);
+            //Push to exist array
             cars[i] = truck[i];
         }
-        for(int i=0;i<b;i++){
-            fold[i].inputInforCar(scanner);
-            cars[i+a] = fold[i];
+        for (int i = 0; i < b; i++) {
+            fold[i] = new Fold();
+            //Call input method
+            fold[i].inputInfoCar(scanner);
+            //Push to exist array
+            cars[i + a] = fold[i];
         }
-        for(int i=0;i<c;i++){
-            seden[i].inputInforCar(scanner);
-            cars[i+a+b] = seden[i];
+        for (int i = 0; i < c; i++) {
+            seden[i] = new Seden();
+            //Call input method
+            seden[i].inputInfoCar(scanner);
+            //Push to exist array
+            cars[i + a + b] = seden[i];
         }
-
-        for(Car car : cars) {
+        for (Car car : cars) {
             car.display();
         }
 
 
     }
-
 
 }
 
